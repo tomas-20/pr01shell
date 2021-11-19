bac0d816d5bb4a025222635150dc7b3d0afb5b8a
tomas-20 <tacuna20@stuy.edu>
2021-11-19 11:47:04 -0500

    created the cheese function

--- input.c
+++ input.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *get_input(int n) {
   char *input = malloc(sizeof (char[n]));
@@ -7,9 +8,21 @@ char *get_input(int n) {
   return input;
 }
 
+void cheese(char *string) {
+  int size = 0;
+  while (string) {
+    char *portion = strsep(&string, " ");
+    printf("(%s)\n", portion);
+    if (*portion) {
+      size ++;
+    }
+  }
+  printf("size is:%d\n", size);
+}
+
 int main() {
   printf("input thingy: ");
   char *bobby = get_input(2187);
-  printf("%s", bobby);
+  cheese(bobby);
   return 0;
 }
