520c803f8d14e9fde92a40cb75abf442557a4332
tomas-20 <tacuna20@stuy.edu>
2021-11-21 13:57:43 -0500

    got rid of the same_string function

--- run.c
+++ run.c
@@ -3,16 +3,15 @@
 #include <stdbool.h>
 #include "input.h"
 
-bool same_string(char *a, char *b) {
-  return a && b && !strcmp(a, b);
-}
-
 void run() {
   bool running = true;
   while (running) {
     char **input = get_input("input thing:", 2187);
-    if (same_string(input[0], "exit")) {
-      running = false;
+    char *command = input[0];
+    if (command) {
+      if (!strcmp(command, "exit")) {
+        running = false;
+      }
     }
     free_strings(input);
   }
