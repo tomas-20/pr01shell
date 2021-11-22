1f3f69d76e6e7094f72796353da25eed1aad84c3
tomas-20 <tacuna20@stuy.edu>
2021-11-22 18:32:47 -0500

    got rid of string_list_node in string_list.h

--- string_list.h
+++ string_list.h
@@ -1,8 +1,3 @@
-struct string_list_node {
-  char *value;
-  int length;
-  struct string_list_node *next;
-};
 struct string_list *get_string_list();
 void free_string_list(struct string_list *);
 void add_string(struct string_list *, char *);
