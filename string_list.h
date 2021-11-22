46db3b2a4149dd7d8eccb7dcbe86be478154324b
tomas-20 <tacuna20@stuy.edu>
2021-11-21 20:39:29 -0500

    created string_list.h

---
+++ string_list.h
@@ -0,0 +1,9 @@
+struct string_list_node {
+  char *value;
+  int length;
+  struct string_list_node *next;
+};
+struct string_list *get_string_list();
+void add_value(struct string_list *, char *);
+void remove_value(struct string_list *);
+char *get_path_string(struct string_list *, char *);
