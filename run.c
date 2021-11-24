8cb1364493b41c1e23eb0e7a91dbd88119d9e3b5
tomas-20 <tacuna20@stuy.edu>
2021-11-23 23:26:22 -0500

    made it so that chdir is used instead of storing the path as a variable

--- run.c
+++ run.c
@@ -2,52 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "string_list.h"
+#include <unistd.h>
 #include "input.h"
 #include "string_split.h"
 
-void show_prompt(char *dir) {
-  if (!dir) {
-    dir = "/";
-  }
-  printf("shelly-shell %s: ", dir);
-}
-
-void cd(struct string_list *path, char *dir) {
-  if (!strcmp(dir, "..")) {
-    remove_string(path);
-  }
-  else if (strcmp(dir, ".")) {
-    add_string(path, dir);
-  }
+void show_prompt() {
+  char *path = getcwd(NULL, 0);
+  printf("shelly-shell %s: ", path);
+  free(path);
 }
 
 void run() {
-  struct string_list *path = get_string_list();
   bool running = true;
   while (running) {
-    show_prompt(get_first_string(path));
+    show_prompt();
     char **input = get_input(2187);
     char *command = input[0];
     if (command) {
       if (!strcmp(command, "exit")) {
         running = false;
       }
-      else if (!strcmp(command, "pwd")){
-        printf("%s\n", get_path_string(path, ""));
-      }
       else if (!strcmp(command, "cd")) {
-        char *arg = input[1];
-        if (!arg || arg[0] == '/') {
-          empty_string_list(path);
-        }
-        if (arg) {
-          char **mini_path = string_split(arg, "/");
-          for (char **sp = mini_path; *sp; sp ++) {
-            cd(path, *sp);
-          }
-          free_strings(mini_path);
-        }
+        char *mini_path = input[1];
+        chdir(mini_path);
       }
     }
     free_strings(input);
